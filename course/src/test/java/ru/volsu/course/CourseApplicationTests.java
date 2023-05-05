fc235dc03404f6ce04dc06dbe144a904f6bb90f9
kur0n0 <shaker1743@gmail.com>
2023-05-06 01:54:47 +0300

    наработки

--- course/src/test/java/ru/volsu/course/CourseApplicationTests.java
+++ course/src/test/java/ru/volsu/course/CourseApplicationTests.java
@@ -3,7 +3,7 @@ package ru.volsu.course;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CourseApplicationTests {
 
 	@Test
