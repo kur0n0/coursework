887b3b958890a16e03e3ba4c4253022acb6e369d
kur0n0 <shaker1743@gmail.com>
2023-05-09 23:28:54 +0300

    корректировка для дебага

--- course/src/main/java/ru/volsu/course/service/ArticleService.java
+++ course/src/main/java/ru/volsu/course/service/ArticleService.java
@@ -5,7 +5,7 @@ import ru.volsu.course.model.Article;
 import java.util.List;
 
 public interface ArticleService {
-    
+
     Article findById(Integer articleId);
 
     void save(Article article);
