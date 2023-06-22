e3fcf66b82d5df5c7beda2664fa2b5540c1d9afa
kur0n0 <shaker1743@gmail.com>
2023-06-23 00:43:26 +0300

    корректировки форматирования

--- course/src/main/java/ru/volsu/course/dao/ArticleRepository.java
+++ course/src/main/java/ru/volsu/course/dao/ArticleRepository.java
@@ -3,14 +3,10 @@ package ru.volsu.course.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.volsu.course.model.Article;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
-//    @Query(value = "SELECT a FROM Article a WHERE a.tag = :tag")
-//    Page<Article> findByTag(@Param(value = "tag") String tag, Pageable pageable);
     Page<Article> findByTag(String tag, Pageable pageable);
 }
